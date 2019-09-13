e77e78ff17f37ce94494016a4261fe3b445dcaf2
Emanuel Joívo <emanueljoivo@gmail.com>
2019-09-13 10:45:59 -0300

    Fixed push to prometheus pushgateway

--- pusher/pusher.go
+++ pusher/pusher.go
@@ -2,12 +2,17 @@ package pusher
 
 import (
 	"log"
+	"strings"
 
 	"github.com/emanueljoivo/telemetry-aggregator/api"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/push"
 )
+
+type Pusher interface {
+	PushMetric(o *Metric)
+}
 
 const (
 	Reachability = "reachability"
@@ -21,26 +26,47 @@ const (
 	ResourceLabel = "resource"
 )
 
-type Pusher interface {
-	PushMetric(o *Metric)
-}
-
 type PrometheusPusher struct{}
 
 func (p PrometheusPusher) PushMetric(m *Metric) {
-	go func() {
+	var metricName = generateMetricName(m)
 
-		metric := prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: m.Name,
-			Help: m.Help,
-		})
+	metric := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:        metricName,
+		Help:        m.Help,
+		ConstLabels: m.Metadata,
+	})
 
-		log.Println(metric)
+	metric.Set(m.Value)
 
-		if err := push.New(api.PushGatewayAddr, "probing_structure").
-			Collector(metric).
-			Push(); err != nil {
-			log.Println("Could not push completion time to Push Gateway: ", err)
-		}
-	}()
+	log.Print("Pushing metric " + metricName + " to the Pushgateway.")
+
+	if err := push.New(api.PushGatewayAddr, "probe_fogbow_stack").
+		Collector(metric).
+		Add(); err != nil {
+		log.Println("Could not push completion time to Pushgateway: ", err)
+	}
+}
+
+func generateMetricName(m *Metric) string {
+	var resultName string
+	const sep = "_"
+
+	switch m.Name {
+	case Reachability:
+		resultName = ServiceLabel + sep + Reachability + sep + strings.ToLower(m.Metadata[ServiceLabel])
+	case SuccessRate:
+		resultName = ResourceLabel + sep + SuccessRate + sep + m.Metadata[ResourceLabel]
+		m.Metadata[ServiceLabel] = "ras"
+	case Latency:
+		resultName = ResourceLabel + sep + Latency + sep + strings.ToLower(m.Metadata[ResourceLabel])
+		m.Metadata[ServiceLabel] = "ras"
+	case Availability:
+		resultName = ResourceLabel + sep + Availability + sep + m.Metadata[ResourceLabel]
+		m.Metadata[ServiceLabel] = "ras"
+	default:
+		resultName = ""
+	}
+
+	return resultName
 }
